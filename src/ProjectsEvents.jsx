21dea8757292998c3a1f83cfdc838e9b30605cf5
sanaeram5 <sanaeram5@gmail.com>
2021-01-01 23:10:50 +0530

    minor changes

--- src/ProjectsEvents.jsx
+++ src/ProjectsEvents.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import e1 from "../src/image/event1.jpg";
+import e2 from "../src/image/event2.jpg";
+import e3 from "../src/image/event3.jpg";
+import e4 from "../src/image/event4.jpg";
 
 const ProjectsEvents = () => {
   return (
@@ -10,7 +14,7 @@ const ProjectsEvents = () => {
           <div className="row">
             <div className="col-xs-12 col-sm-6 col-lg-3">
               <div className="zoom">
-                <img src="https://picsum.photos/200/300" alt="" />
+                <img src={e1} alt="" />
                 <div className="caption">
                   <h4>Lorem</h4>
                   <p>
@@ -22,7 +26,7 @@ const ProjectsEvents = () => {
             </div>
             <div className=" col-xs-12 col-sm-6 col-lg-3">
               <div className="zoom">
-                <img src="https://picsum.photos/300/300" alt="" />
+                <img src={e2} alt="" />
                 <div className="caption">
                   <h4>Lorem</h4>
                   <p>
@@ -34,7 +38,7 @@ const ProjectsEvents = () => {
             </div>
             <div className=" col-xs-12 col-sm-6 col-lg-3">
               <div className="zoom">
-                <img src="https://picsum.photos/500/300" alt="" />
+                <img src={e3} alt="" />
                 <div className="caption">
                   <h4>Lorem</h4>
                   <p>
@@ -46,7 +50,7 @@ const ProjectsEvents = () => {
             </div>
             <div className="col-xs-12 col-sm-6 col-lg-3">
               <div className="zoom">
-                <img src="https://picsum.photos/400/300" alt="" />
+                <img src={e4} alt="" />
                 <div className="caption">
                   <h4>Lorem</h4>
                   <p>
@@ -63,7 +67,7 @@ const ProjectsEvents = () => {
           <div className="row hi">
             <div className="col-xs-12 col-sm-6 col-lg-3">
               <div className="zoom">
-                <img src="https://picsum.photos/200/350" alt="" />
+                <img src={e4} alt="" />
                 <div className="caption">
                   <h4>Lorem</h4>
                   <p>
