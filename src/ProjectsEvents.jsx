7f4eaa14d614c7885aed31020e4c941588290c21
Saba Sarwar <sabasarwar1997@gmail.com>
2021-01-02 23:03:38 +0530

    home projects modified

--- src/ProjectsEvents.jsx
+++ src/ProjectsEvents.jsx
@@ -56,10 +56,12 @@ const ProjectsEvents = () => {
               <div className="zoom">
                 <img src="https://s27389.pcdn.co/wp-content/uploads/2019/08/four-unexpected-blockchain-use-cases.jpeg" alt="" />
                 <div className="caption">
-                  <h6>Q blogging</h6>
-                  <p>
-                  Web application which emphasis on the quality of blogging by implementing partial concepts of blockchain & Social networking sites{" "}
-                  </p>
+                  <h6>QBlogging</h6>
+                  <div className="project-desc">
+                    <p>
+                    Web application which emphasis on the quality of blogging by implementing partial concepts of blockchain & Social networking sites{" "}
+                    </p> 
+                  </div>  
                   <button type="button" class="btn btn-outline-dark btn-sm"><a href="https://github.com/aryabhatta22/Qblogging.git">Source Code</a></button>
                 </div>
               </div>
@@ -68,9 +70,11 @@ const ProjectsEvents = () => {
               <div className="zoom">
                 <img src="https://avatars3.githubusercontent.com/u/10216045?s=400&v=4" alt="" />
                 <div className="caption">
-                  <h6>mlpack</h6>
-                  <p>
-                  mlpack: a scalable C++ machine learning library</p>
+                  <h6>MLpack</h6>
+                  <div className="project-desc">
+                    <p>
+                    MLpack, A scalable C++ machine learning library</p>
+                  </div> 
                   <button type="button" class="btn btn-outline-dark btn-sm"><a href="https://github.com/zjaweds/mlpack.git">Source Code</a></button>
                 </div>
               </div>
@@ -80,8 +84,9 @@ const ProjectsEvents = () => {
                 <img src="https://dhanjivpandey.com/wp-content/uploads/2018/05/php.jpg" alt="" />
                 <div className="caption">
                   <h6>Hall Allotment System</h6>
-                  <p>
-                  Hall Allotment System for my hostel : Indira Gandhi Hall</p>
+                  <div className="project-desc">
+                    <p>Hall Allotment System for my hostel, Indira Gandhi Hall</p>
+                  </div>
                   <button type="button" class="btn btn-outline-dark btn-sm"><a href="https://github.com/sabasarwar/Hall-Allotment-System.git">Source Code</a></button>
                 </div>
               </div>
@@ -90,10 +95,12 @@ const ProjectsEvents = () => {
               <div className="zoom">
                 <img src="https://miro.medium.com/max/3840/1*opzxrBna63YDbd8_pM5trw.png" alt="" />
                 <div className="caption">
-                  <h6 style={{fontSize:"22px",fontWeight:"bold"}}>Stock Exchange Prediction</h6>
-                  <p>
-                  The model predicts whether the stock prices of a company are going to be Up or Down based on the historical performance of its stock.</p>
-                  <button type="button" class="btn btn-outline-dark btn-sm"><a href="https://github.com/aryabhatta22/Stock-Exchange-Prediction-using-CNN.git">Source Code</a></button>
+                  <h6>Stock Exchange Prediction</h6>
+                  <div className="project-desc">
+                    <p>
+                    The model predicts whether the stock prices of a company are going to be Up or Down based on the historical performance of its stock.</p>
+                  </div>
+                 <button type="button" class="btn btn-outline-dark btn-sm"><a href="https://github.com/aryabhatta22/Stock-Exchange-Prediction-using-CNN.git">Source Code</a></button>
                 </div>
               </div>
             </div>
